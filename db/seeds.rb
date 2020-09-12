13a25826cdc28970621513c805f2a423003f768e
いず <maybe05.maggio@gmail.com>
2020-09-13 00:26:09 +0900

    中級編DAY10 記事の表示

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# ダミーデータを作るためのページ
+
+# terminalで rails db:seedと入力すると実行できるよ
+10. times do
+Article.create(
+  title: Faker::Lorem.sentence(word_count: 5),
+  content: Faker::Lorem.sentence(word_count: 100)
+)
+end
