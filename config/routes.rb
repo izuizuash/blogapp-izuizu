57ec251d3e1760511013ba2673c9566e92a9f1c2
いず <maybe05.maggio@gmail.com>
2020-09-23 22:09:51 +0900

    記事を更新できるように変更

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'articles#index'
   get '/' => 'articles#index'
 # resources便利 URL作ってくれる
-  resources :articles, only: [:show, :new, :create]
+  resources :articles, only: [:show, :new, :create, :edit, :update]
 end
