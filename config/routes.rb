5128fa612891b13cec91c49a05bc10bd883db1f3
いず <maybe05.maggio@gmail.com>
2020-10-01 22:31:41 +0900

    show comment

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   get '/' => 'articles#index'
 
 # resources便利 URL作ってくれる
-  resources :articles
-
+  resources :articles do
+    resources :comments, only: [:new, :create]
+  end
+# articleがあって、その下にcommentsがあるって関係のときは、上記のように入れ子構造に
 end
